c8c8cd332ef14b04e178aa4b486e83a40e7f228f
Paulo Henrique <paulo_phs01@hotmail.com>
2023-03-06 15:26:54 -0300

    for loop

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ import chalk from 'chalk'
 
 const prompt = PromptSync();
 
-let valor1 = Number(prompt("Digite o valorr 1: "))
-let valor2 = Number(prompt("Digite o valor 2: "))
-let valor3 = Number(prompt("Digite o valor 3: "))
-let valor4 = Number(prompt("Digite o valor 4: "))
-let valor5 = Number(prompt("Digite o valor 5: "))
+let valor = 0
+let media = 0
 
-const media = (valor1 + valor2 + valor3 + valor4 + valor5)/5
+for (let i = 0; i < 5; i++) {
+    valor += Number(prompt("Digite o valor: "))
+}
 
-
+media = valor / 5
 
 console.log(chalk.bgBlue("A média: " + media))
